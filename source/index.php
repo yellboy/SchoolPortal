1cd5536e98aae18614f4122d71eb3e5a69cf6277
yellboy <bojan.jelaca@yahoo.com>
2015-03-08 16:31:36 +0100

    Added users page

--- source/index.php
+++ source/index.php
@@ -19,7 +19,8 @@
 				<div class="col-md-7">
 					<img src="styles/img/rss.png" alt="RSS" />
 					<ul>
-						<li><a href="login.html" title="Prijava na sistem">пријави ме</a></li>
+						<li><a href="login.html" title="Prijava na sistem">Пријави ме</a></li>
+						<li ><a href="users.html" title="Pretraga korisnika">Претрага корисника</a></li>
 						<li class="auth-only">Marin Markic - </li>
 						<li class="auth-only"><a href="login.html" title="Odjava sa sistema">одјави ме</a></li>
 					</ul>
