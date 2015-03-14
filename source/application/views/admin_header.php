d2aadd0c03bec5fb4a94545103ae7a584842b55a
Marin Markic <mane90bg@gmail.com>
2015-03-14 14:25:56 +0100

    Category and articles administration improvement.

--- source/application/views/admin_header.php
+++ source/application/views/admin_header.php
@@ -5,7 +5,7 @@
 				<li><a href="/default_controller" title="Prijava na sistem">Почетна</a></li>
 				<li><a href="/administration/users" title="Pretraga korisnika">Kорисници</a></li>
 				<li><a href="/administration/category" title="Pretraga korisnika">Категорије</a></li>
-				<li><a href="/administration/category" title="Pretraga korisnika">Чланци</a></li>
+				<li><a href="/administration/article" title="Pretraga korisnika">Чланци</a></li>
 				<li><a href="/administration/register" title="Pretraga korisnika">Мој профил</a></li>
 				<li class="auth-only">Marin Markic - </li>
 				<li class="auth-only"><a href="login.html" title="Odjava sa sistema">одјави ме</a></li>
@@ -14,9 +14,9 @@
 		<div class="col-md-4">
 			<form id="searchbox" class="form-inline">
 				<div class="form-group">
-					<input id="search" type="text" class="form-control" placeholder="Унесите текст">
+					<input id="search" type="text" class="form-control" placeholder="Унесите текст" style="width: auto;">
 				</div>
-				<div class="form-group" align="right">
+				<div class="form-group">
 					<button id="submit" type="submit">Претражи</button>
 				</div>
 			</form>
--- source/assets/scripts/article.js
+++ source/assets/scripts/article.js
@@ -1,6 +1,13 @@
-$(function() {
+$(function () {
 
-	var $jsTree = $('#jstree_demo')
+	var model = {
+		categoryId: 0,
+		categoryName: '',
+		searchByName: '',
+		searchByAuthor: ''
+	};
+
+	var $jsTree = $('#jstree-holder')
 		.jstree({
 			"core" : {
 				"animation" : 0,
@@ -24,9 +31,50 @@ $(function() {
 			"plugins" : [ "dnd", "search", "state", "types", "wholerow" ],
 		});
 
-		//define on selected callback
+	$('#jstree-holder').on('changed.jstree', function (e, data) {
+		if (data.selected.length > 0) {
+			model.categoryId = data.selected[0];
+			model.categoryName = data.instance.get_node(data.selected[0]).text;
+
+			if (model.categoryId == 1){
+				$('.article-list').hide();
+				$('.article-details').hide();
+				return;
+			}
+
+			$('span.category-name').html(model.categoryName);
+
+			//if (model.categoryId == 2) {
+			//	$('span.section-title').html('Vesti');
+			//}
+			//else {
+			//	$('span.section-title').html('Clanci');
+			//}
+
+			// MM4MM reload article list - ajax, probably use handlebars, send model to server
+			// set category name, hide editor
+			$('.article-list').show();
+			$('.article-details').hide();
+		}
+	});
+
+	$('body').on('click', '.add-article', function (e) {
+		$('.article-details').show();
+		// init new
+	});
+
+	$('body').on('click', '.edit-article', function (e) {
+		$('.article-details').show();
+		// load article data
+	});
+
+	$('body').on('click', '.delete-article', function (e) {
+		// confirm popup, isDelete true ajax call async
+		$('.article-details').hide();
+		$(e.currentTarget).parents('tr').remove();
+	});
 
 	CKEDITOR.replace('ck-holder', {
-   		language: 'sr'
+		language: 'sr'
 	});
 });
