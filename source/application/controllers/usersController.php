22520a79dea93ea8e1785c16e72b0b6388d35396
Marin Markic <mane90bg@gmail.com>
2015-06-03 20:55:12 +0200

    Few fixes.

--- source/application/controllers/usersController.php
+++ source/application/controllers/usersController.php
@@ -10,16 +10,22 @@ class UsersController extends CI_Controller {
 	
 	public function index()
 	{
-		$data['users'] = $this::loadUsers();
-		$session_data = $this->session->userdata('logged_in');
-		$data['username'] = $session_data['username'];
-		$data['id'] = $session_data['id'];
-		$this->load->view('layouts/usersLayout', $data);
+		if(RequireAuthentication())
+		{	
+			$data['users'] = $this::loadUsers();
+			$session_data = $this->session->userdata('logged_in');
+			$data['username'] = $session_data['username'];
+			$data['id'] = $session_data['id'];
+			$this->load->view('layouts/usersLayout', $data);
+		}
 	}
 	
 	private function loadUsers() 
 	{
-		return $this->Users_model->getUsers();
+		if(RequireAuthentication())
+		{	
+			return $this->Users_model->getUsers();
+		}
 	}
 	
 	public function AddNewUser() 
@@ -45,6 +51,3 @@ class UsersController extends CI_Controller {
 		}
 	}
 }
-
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
